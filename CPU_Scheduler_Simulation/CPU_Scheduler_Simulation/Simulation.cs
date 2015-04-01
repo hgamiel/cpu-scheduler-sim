95456d61179f06c01f07dca4a5681b8db029d3d8
Hannah Gamiel <hgamiel15@my.whitworth.edu>
2015-04-01 10:01:42 -0700

    Cleaned up the code by making another function just for adding processes.

--- CPU_Scheduler_Simulation/CPU_Scheduler_Simulation/Simulation.cs
+++ CPU_Scheduler_Simulation/CPU_Scheduler_Simulation/Simulation.cs
@@ -33,27 +33,7 @@ namespace CPU_Scheduler_Simulation
                 {
                     var line = reader.ReadLine(); // reads in line
                     var values = line.Split(new string[] {"\t"}, StringSplitOptions.RemoveEmptyEntries); // creates array of values in line
-
-                    PCB process = new PCB(); // the new process we will add to our list
-                    process.PID = Convert.ToInt32(values[0]); // process PID
-                    process.priorityNumber = Convert.ToInt32(values[1]); // process priority number
-                    process.arrivalTime = Convert.ToDouble(values[2]); // process arrival time
-
-                    for (int i = 3; i < values.Length; i++) // since bursts start at the 4th column (index 3 in an array), we start reading in bursts there
-                    {
-                        var burst = Convert.ToInt32(values[i]); // burst time
-                        if (i % 2 != 0) // if in an odd column, then it's a CPU burst
-                        {
-                            process.CPU.Add(burst); // add CPU burst to the process' CPU burst list
-                        }
-                        else // then it's in an even column, so it's an I/O burst
-                        {
-                            process.IO.Add(burst); // add I/O burst to the process' I/O burst list
-                        }
-                    }
-
-                    processTable.Add(process);
-                    Console.WriteLine("Process with PID {0} added.", process.PID);
+                    addProcess(values); // calls function that adds process to process table
                 }
             }
             else
@@ -61,6 +41,28 @@ namespace CPU_Scheduler_Simulation
                 Console.WriteLine("File {0} not found.", filename);
             }
         }        //pushes info from lines on .dat files into processTable as a PCB
+
+        public void addProcess(string [] values) {
+            PCB process = new PCB(); // the new process we will add to our list
+            process.PID = Convert.ToInt32(values[0]); // process PID
+            process.priorityNumber = Convert.ToInt32(values[1]); // process priority number
+            process.arrivalTime = Convert.ToDouble(values[2]); // process arrival time
+
+            for (int i = 3; i < values.Length; i++) // since bursts start at the 4th column (index 3 in an array), we start reading in bursts there
+            {
+                var burst = Convert.ToInt32(values[i]); // burst time
+                if (i % 2 != 0) // if in an odd column, then it's a CPU burst
+                {
+                    process.CPU.Add(burst); // add CPU burst to the process' CPU burst list
+                }
+                else // then it's in an even column, so it's an I/O burst
+                {
+                    process.IO.Add(burst); // add I/O burst to the process' I/O burst list
+                }
+            }
+            processTable.Add(process); // adds process to process table
+            Console.WriteLine("Process with PID {0} added.", process.PID);
+        }
 
         public void startSim(int quantum) {
             this.quantum = quantum; // sets quantum for the simulation (should be 1 or 2; some very small number)
