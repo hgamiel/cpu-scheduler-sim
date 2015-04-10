25a4736fe839880a0c3ad86d409e490d4d11a2e7
Hannah Gamiel <hgamiel15@my.whitworth.edu>
2015-04-09 23:47:40 -0700

    Started on Statistics class

---
+++ CPU_Scheduler_Simulation/CPU_Scheduler_Simulation/Statistics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Scheduler_Simulation
+{
+    public class Statistics
+    {
+        public List<PCB> finishedProcesses = new List<PCB>();
+
+        public Statistics() { }
+
+        public double determineAverageTRTS() {
+
+            double sum = 0;
+            double average;
+            double countTRTS = 0;
+
+            foreach (var p in finishedProcesses)
+            {
+                foreach (var t in p.tr_ts)
+                {
+                    sum += t;
+                    countTRTS++;
+                }
+            }
+
+            average = (sum > 0) ? sum / countTRTS : 0;
+
+            return average;
+        }
+
+    }
+}
